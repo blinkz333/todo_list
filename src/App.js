269797ec3524d731dc08ea915434df12a7ca600a
blink <threerapon.rat@gmail.com>
2023-04-23 23:55:36 +0700

    fix route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  Switch
 } from 'react-router-dom';
 import Todolist from './components/todolist';
 import Fetch_Data from './components/fetchdata';
@@ -11,8 +10,8 @@ const  App = () => {
   return (
     <BrowserRouter >
     <Routes>
-      <Route exec path="/todo_list" exact Component={<Todolist />} />
-      <Route path="/todo_list/fetch_data" exact  Component={<Fetch_Data />} />
+      <Route exec path="/todo_list" exact element={<Todolist />} />
+      <Route path="/todo_list/fetch_data" exact  element={<Fetch_Data />} />
     </Routes>
   </BrowserRouter>
   );
